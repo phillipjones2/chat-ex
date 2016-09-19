371c1b18a8d7854839754bda0832172a95e5ab6a
Phillip Jones <phillipemersonjones@gmail.com>
2016-09-19 14:03:40 -0500

    add socket io

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res) {
-  res.send('<h1>Hello World</h1>')
+  res.sendFile(__dirname + '/index.html')
 })
 
 http.listen(3000, function() {
