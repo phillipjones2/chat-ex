349566c9a45de68b2fbaad967883778fc6938324
Phillip Jones <phillipemersonjones@gmail.com>
2016-09-19 21:19:03 -0500

    created components for login and register

---
+++ src/comps/LoginForm/LoginForm.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router'
+import style from './LoginFrom.styl'
+
+export default class LoginForm extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+  render() {
+    return(
+      <div className="LoginForm">
+
+      </div>
+    )
+  }
+}
---
+++ src/comps/RegisterForm/RegisterForm.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router'
+import style from './RegisterForm.styl'
+
+export default class RegisterForm extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+  render() {
+    return(
+      <div className="RegisterForm">
+        
+      </div>
+    )
+  }
+}
